7c93af82181428bc633ec08db2086d268ae117da
Vlad Stan <stan.v.vlad@gmail.com>
2017-01-05 16:18:49 +0200

    added intenditLink sort also for test testGetIdentityLinksWithNonExistingOwner()

--- modules/flowable-engine/src/test/java/org/flowable/engine/test/api/task/TaskServiceTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/engine/test/api/task/TaskServiceTest.java
@@ -1040,12 +1040,19 @@ public class TaskServiceTest extends PluggableFlowableTestCase {
     List<IdentityLink> identityLinks = taskService.getIdentityLinksForTask(taskId);
     assertEquals(2, identityLinks.size());
 
-    IdentityLink assignee = identityLinks.get(1);
+    identityLinks.sort(new Comparator<IdentityLink>() {
+      @Override
+      public int compare(IdentityLink identityLink, IdentityLink identityLink2) {
+        return identityLink.getUserId().compareTo(identityLink2.getUserId());
+      }
+    });
+    
+    IdentityLink assignee = identityLinks.get(0);
     assertEquals("nonExistingAssignee", assignee.getUserId());
     assertNull(assignee.getGroupId());
     assertEquals(IdentityLinkType.ASSIGNEE, assignee.getType());
 
-    IdentityLink owner = identityLinks.get(0);
+    IdentityLink owner = identityLinks.get(1);
     assertEquals("nonExistingOwner", owner.getUserId());
     assertNull(owner.getGroupId());
     assertEquals(IdentityLinkType.OWNER, owner.getType());
